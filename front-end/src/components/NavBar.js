30cdea66f513b85e644e70f3d0ea2794cd6edfe7
gcampbell391 <gcampbell391@gmail.com>
2020-05-03 19:23:19 -0400

    gene4

--- front-end/src/components/NavBar.js
+++ front-end/src/components/NavBar.js
@@ -1,9 +1,16 @@
 import React from "react"
-
+import { faHome } from "@fortawesome/free-solid-svg-icons";
+import { faUserCircle } from "@fortawesome/free-solid-svg-icons";
+import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 const NavBar = (props) => {
     return (
         <div className="navBar">
-
+            <ul className="iconList" >
+                <li><FontAwesomeIcon icon={faHome} style={{ color: 'white' }} size="2x" /></li>
+                <li><FontAwesomeIcon icon={faUserCircle} style={{ color: 'white' }} size="2x" /></li>
+                <li><FontAwesomeIcon icon={faShoppingCart} style={{ color: 'white' }} size="2x" /></li>
+            </ul>
             <img className="cartLogo" src={require("../images/cart_icn.png")} height="30" width="30" onClick={props.handleCartClick}></img>
 
         </div>
